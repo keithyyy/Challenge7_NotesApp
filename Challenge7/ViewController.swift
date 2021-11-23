ed3f6de1fc5fa12252e8c668b23dc785d84a11a7
Keith Crooc <keith.crooc@gmail.com>
2021-11-22 22:11:33 -0500

    Setting up for Challenge7, created notes for guidance

--- Challenge7/ViewController.swift
+++ Challenge7/ViewController.swift
@@ -5,15 +5,30 @@
 //  Created by Keith Crooc on 2021-11-21.
 //
 
+
+// IMITATING THE NOTES APP
+
+// 1. Tableview to list notes. Embed in a navigation controller.
+// 2. Tapping a note should slide in a detailView -> contains a full-screen text view (project 19)
+// 3. Save each note as it's own class? Should be done using codable.
+// 4. Add some tool bar items in the DetailView controller to delete / compose.
+// 5. add An action button that shares the text using UIActivityViewController.
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Notes"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+    
+    
+    
+    
+    
+    
 
 }
 
