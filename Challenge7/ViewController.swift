b253985169c9aa9af940d9268660cde2b2c472f5
Keith Crooc <keith.crooc@gmail.com>
2021-11-29 20:57:14 -0500

    when you click on a note (for now the demo note) it'll open with it's saved data. The note title will be the first line and also be Bolded

--- Challenge7/ViewController.swift
+++ Challenge7/ViewController.swift
@@ -9,7 +9,7 @@
 // IMITATING THE NOTES APP
 
 // 1. Tableview to list notes. Embed in a navigation controller. ✅
-// 2. Tapping a note should slide in a detailView -> contains a full-screen text view (project 19)
+// 2. Tapping a note should slide in a detailView -> contains a full-screen text view (project 19) ✅
 // 3. Save each note as it's own class? Should be done using codable. (Project 12)
 // 4. Add some tool bar items in the DetailView controller to delete / compose. (Project 4)
 // 5. add An action button that shares the text using UIActivityViewController. (Project 3)
